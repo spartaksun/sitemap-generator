fb8d67ecfe52648b0017b3e17355df4d9b4461d2
a.belyakovskiy <2697024@gmail.com>
2015-03-27 16:33:54 +0200

    Added mysql to provision

--- app/web/index.php
+++ app/web/index.php
@@ -3,5 +3,14 @@ ini_set('display_errors', 1);
 require "../vendor/autoload.php";
 
 $generator = new \spartaksun\sitemap\generator\Generator();
+$generator->storage = new \spartaksun\sitemap\generator\storage\MysqlStorage('qwerty');
+$generator->storage->db = [
+    'name' => 'sitemap',
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => '',
+];
+$generator->loader  = new \spartaksun\sitemap\generator\loader\Loader();
+
 $generator->generate('http://facebook.com');
 
