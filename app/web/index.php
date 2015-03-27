37d8c712fc70b3e373fcf8a1e0d19a7af54bf4cd
a.belyakovskiy <2697024@gmail.com>
2015-03-28 01:02:34 +0200

    Some IOC

--- app/web/index.php
+++ app/web/index.php
@@ -2,15 +2,21 @@
 ini_set('display_errors', 1);
 require "../vendor/autoload.php";
 
-$generator = new \spartaksun\sitemap\generator\Generator();
-$generator->storage = new \spartaksun\sitemap\generator\storage\MysqlStorage('qwerty');
-$generator->storage->db = [
-    'name' => 'sitemap',
-    'host' => 'localhost',
-    'user' => 'root',
-    'pass' => '',
-];
-$generator->loader  = new \spartaksun\sitemap\generator\loader\Loader();
+$generator = new \spartaksun\sitemap\generator\Generator('http://jewelport.net');
 
-$generator->generate('http://facebook.com');
+session_start();
+
+$generator->loader          = new \spartaksun\sitemap\generator\loader\Loader();
+$generator->worker          = new \spartaksun\sitemap\generator\SiteWorker();
+$generator->worker->parser  = new \spartaksun\sitemap\generator\parser\HtmlParser();
+$generator->storage         = new \spartaksun\sitemap\generator\storage\MysqlStorage('ttttt');
+$generator->storage->db     = [
+                                    'name' => 'sitemap',
+                                    'host' => 'localhost',
+                                    'user' => 'root',
+                                    'pass' => '',
+                                ];
+$generator->level = 5;
+
+$generator->generate();
 
